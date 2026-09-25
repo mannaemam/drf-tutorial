4cfe7bb69b02c1fe93532738b867e2069b6ad54a
mannaemam <mannaemam@gmail.com>
2026-09-25 14:50:14 +0600

    added snippet detail view

--- snippets/views.py
+++ snippets/views.py
@@ -14,6 +14,7 @@ def snippet_list(request):
         snippets = Snippet.objects.all()
         serializer = SnippetSerializer(snippets, many=True)
         return JsonResponse(serializer.data, safe=False)
+
     elif request.method == 'POST':
         data = JSONParser.parse(request)
         serializer = SnippetSerializer(data=data)
@@ -21,3 +22,30 @@ def snippet_list(request):
             serializer.save()
             return JsonResponse(serializer.data, status=201)
         return JsonResponse(serializer.errors, status=400)
+
+
+@csrf_exempt
+def snippet_detail(request, slug):
+    """
+    Retrieve, update or delete a code snippet.
+    """
+    try:
+        snippet = Snippet.objects.get(slug=slug)
+    except Snippet.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = SnippetSerializer(snippet)
+        return JsonResponse(serializer.data)
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = SnippetSerializer(snippet, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+    
+    elif request.method == 'DELETE':
+        snippet.delete()
+        return HttpResponse(status=204)
